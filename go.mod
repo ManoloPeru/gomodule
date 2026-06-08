0d6e0d7309d95599249499c658146ed60bed0ba4
Manuel H. Chávez Castro <manuel.chc@gmail.com>
2026-06-08 11:35:51 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ManoloPeru/gomodule
+
+go 1.26.3
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+func HelloWorld() {
+	// Para que la función pueda ser consumoda desde otros paquetes, el nombre de la función debe comenzar con mayúscula.
+	fmt.Println("Hola desde GO...")
+}
